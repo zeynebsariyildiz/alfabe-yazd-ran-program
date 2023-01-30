94230a344cdd776c902730cf057d546741a4ec0d
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-30 18:05:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alfabe_yazdıran_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+            for (i = 'a'; i <= 'z'; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
